f25dee9f9ca872e4be011a5d4baca329fe6b6733
RuslanKammerer <ruslankammerer@gmail.com>
2023-04-24 21:44:51 +0500

    Урок 1. Тест 3

--- MainClassTest.java
+++ MainClassTest.java
@@ -1,10 +1,20 @@
 import org.junit.Assert;
 import org.junit.Test;
-public class MainClassTest {
-    MainClass n = new MainClass();
+public class MainClassTest extends MainClass {
+   String str = this.getClassString();
+   int index1 = str.lastIndexOf("Hello");
+   int index2 =str.lastIndexOf("hello");
     @Test
-    public void testGetClassNumber() {
-        Assert.assertTrue("Полученное число не больше 40",n.getClassNumber()>45);
+    public void testGetClassString() {
+        if (index1 != -1){
+            System.out.println("Искомая подстрока найдена");
     }
-
+        else if (index2 != -1) {
+            System.out.println("Искомая подстрока найдена");
+        }
+        else {
+            System.out.println("Искомая подстрока не найдена");
+            Assert.fail();
+        }
+}
 }
